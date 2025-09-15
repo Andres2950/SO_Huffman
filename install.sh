c6dc17819a7a53723b14cebb3429306a8eb830f2
gerald <gerald@DebianSO.myguest.virtualbox.org>
2025-09-14 21:21:04 -0600

    Archivo para instalar gcc

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+user=$(logname)
+DIR=$(pwd)
+
+if [ "$EUID" -ne 0 ]; then
+  echo "This script requires root permissions."
+  echo "It will be executed again with su -c"
+  exec su -c "$0"
+fi
+
+echo "Installign GCC.."
+
+install_gcc(){
+  if command -v gcc &>/dev/null; then
+    echo "GCC is already installed. Version: $(gcc --version | head -n1)"
+  else
+    if command -v apt &>/dev/null; then
+      apt update
+      apt install -y gcc
+    fi
+  fi
+}
+
+install_gcc
+su - "$user" -c "cd '$DIR' && chmod +x build/main && ./build/main"
+#su - "$user" -c "./build/main"
